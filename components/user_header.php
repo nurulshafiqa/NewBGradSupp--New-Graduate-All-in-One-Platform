5de40ad5a87d2d3cce16a7fd081281a4e495840f
nurulshafiqa <95677589+nurulshafiqa@users.noreply.github.com>
2023-03-18 14:52:53 +0800

    Update user_header.php

--- components/user_header.php
+++ components/user_header.php
@@ -1,1 +1,59 @@
+<header class="header" style="padding: 0.1px 1px;">
 
+   <section class="flex">
+   
+   <!-- <div class="icons">
+         <div id="menu-btn" class="fas fa-bars" style="margin-left:10rem;"></div>
+         
+      </div> -->
+         
+      
+
+      
+   <img src="images/logonew.png" alt="logo" height="45" style="margin-right:950px;" />
+ 
+   <!-- <a href="" class="logo" style="font-size: 14px;margin-right:1000px;">NewBGradSupp</a>  -->
+
+    
+      <div class="icons">
+         <div id="menu-btn" class="fas fa-bars"></div>
+         <div id="search-btn" class="fas fa-search"></div>
+         <div id="user-btn" class="fas fa-user"></div>
+        
+      </div>
+
+      <div class="profile">
+        
+         <div class="flex-btn">
+            <a href="login.php" class="option-btn">login</a>
+            <a href="register.php" class="option-btn">register</a>
+         </div>
+       
+      </div>
+
+   </section>
+
+</header>
+
+
+
+<div class="side-bar" style="background-color:purple; height: 100vh;
+   width: 20rem;">
+
+   <div class="close-side-bar">
+      <i class="fas fa-times"></i>
+   </div>
+
+ 
+
+   <nav class="navbar" >
+      <a href="" ><i class="fas fa-home" style="color:white;"></i><span style="color:white;font-size:14px;">Home</span></a>
+      <a href=""><i class="fa fa-briefcase"  style="color:white;"></i><span style="color:white;font-size:14px;">List of Job</span></a>
+      <a href=""><i class="fa fa-calculator"  style="color:white;"></i><span style="color:white;font-size:14px;">Salary Comparison</span></a>
+      <a href=""><i class="fas fa-chalkboard-user"  style="color:white;"></i><span style="color:white;font-size:14px;">Training Recommendation</span></a>
+      <a href=""><i class="fas fa-question"  style="color:white;"></i><span style="color:white;font-size:14px;">About us</span></a>
+      <a href=""><i class="fas fa-headset"  style="color:white;"></i><span style="color:white;font-size:14px;">Contact us</span></a>
+      <a href=""><i class="fa fa-sign-out"  style="color:white;"></i><span style="color:white;font-size:14px;">Sign Out</span></a>
+   </nav>
+
+</div>
